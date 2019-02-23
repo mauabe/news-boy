cecca3c35f2b37c79293f65aae8402d477b06ec4
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-02-23 00:33:19 -0800

    connect front end

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const request = require('request');
-const stories = require('./stories');
+// const stories = require('./stories');
 const path = require ('path');
 const PORT = 3000;
 
@@ -8,8 +8,8 @@ const PORT = 3000;
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Request details. Method: ', req.method, 'Original url: ', req.originalUrl);
-
+  console.log('Request details. Method: ', req.method);
+  console.log('Original url: ', req.originalUrl);
   next();
 });
 
@@ -23,15 +23,15 @@ app.use(express.static(path.join(__dirname, 'client/dist')));
 app.get('/ping', (req, res) => {
   res.send('pong!');
 });
-app.get('/stories', (req, res) => {
-  res.json(stories);
-});
+// app.get('/stories', (req, res) => {
+//   res.json(stories);
+// });
 
-app.get('/stories/:title', (req, res) => {
-  const {title } = req.params;
+// app.get('/stories/:title', (req, res) => {
+//   const {title } = req.params;
 
-  res.json(stories.filter (story => story.title.includes(title)))
-});
+//   res.json(stories.filter (story => story.title.includes(title)))
+// });
 
 app.get('/topstories', (req, res, next) => {
 
@@ -43,18 +43,20 @@ app.get('/topstories', (req, res, next) => {
       }
 
       const topStories = JSON.parse(body);
-      const limit = 10;
+
+      // console.log('topStories: ', topStories); //array of storyIDs
+      const limit = 4;
 
       res.json(
-        topStories.slice(0, limit).map(story => (
+        topStories.slice(0, limit).map(storyId => (
           request(
-            { url: `https://hacker-news.firebaseio.com/v0/item/${story}.json` },
+            { url: `https://hacker-news.firebaseio.com/v0/item/${storyId}.json` },
             (error, response, body) => {
               if (error || response.statusCode !== 200) {
                 return next(new Error('Error requesting story item'));
               }
 
-              console.log('JSON.parse(body)', JSON.parse(body));
+              console.log('JSON.parse(body) LIMIT 4: ', JSON.parse(body));
 
               return JSON.parse(body);
             }
